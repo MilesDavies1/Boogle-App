1c66ffd67068763383e252c60538d825e601ddae
MilesDavies1 <104517485+MilesDavies1@users.noreply.github.com>
2024-10-10 17:55:27 -0400

    Update boggle_solver.py

--- boggle_solver.py
+++ boggle_solver.py
@@ -4,7 +4,7 @@ class Boggle:
         self.grid = grid if grid is not None else []
         self.dictionary = set(dictionary) if dictionary is not None else set()
         self.solution = set()
-        self.visited = []
+        self.visited = {}
 
     def setGrid(self, grid):
         self.grid = grid
@@ -21,16 +21,18 @@ class Boggle:
         rows = len(self.grid)
         cols = len(self.grid[0])
 
-        self.visited = [[False] * cols for _ in range(rows)]
+        # Reset visited for each call
+        self.visited = {}
 
         for r in range(rows):
             for c in range(cols):
+                #zero out grid here
                 self._dfs(r, c, "", self.dictionary)
 
         return list(self.solution)
 
     def _dfs(self, r, c, current_word, words_set):
-        if self.visited[r][c]:
+        if (r, c) in self.visited:
             return
 
         current_word += self.grid[r][c]
@@ -40,16 +42,16 @@ class Boggle:
             self.solution.add(current_word)
 
         # Mark the cell as visited
-        self.visited[r][c] = True
+        self.visited[(r, c)] = True
 
         # Explore all 8 possible directions
         for dr, dc in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
             nr, nc = r + dr, c + dc
-            if self._is_within_bounds(nr, nc) and not self.visited[nr][nc]:
+            if self._is_within_bounds(nr, nc) and (nr, nc) not in self.visited:
                 self._dfs(nr, nc, current_word, words_set)
 
-        # Backtrack by marking the cell as unvisited
-        self.visited[r][c] = False
+        # Backtrack by removing the cell from visited
+        del self.visited[(r, c)]
 
     def _is_within_bounds(self, r, c):
         return 0 <= r < len(self.grid) and 0 <= c < len(self.grid[0])
@@ -83,3 +85,16 @@ if __name__ == "__main__":
     dictionary4 = ["AB", "BCD", "CFI", "E", "DE", "GHI", "EF", "ADG", "BCE"]
     boggle4 = Boggle(grid4, dictionary4)
     print(boggle4.getSolution())  # Output: ['AB', 'BCD', 'CFI', 'DE', 'GHI', 'EF', 'ADG', 'BCE']
+
+# class Boggle:
+#     def __init__(self, grid, dictionary):
+#         self.grid = grid
+#         self.dictionary = dictionary
+#         self.solutions = []
+
+# def main():
+#     grid = [["T", "W", "Y", "R"], ["E", "N", "P", "H"],["G", "Z", "Qu", "R"],["O", "N", "T", "A"]]
+#     dictionary = ["art", "ego", "gent", "get", "net", "new", "newt", "prat", "pry", "qua", "quart", "quartz", "rat", "tar", "tarp", "ten", "went", "wet", "arty", "rhr", "not", "quar"]
+    
+#     mygame = Boggle(grid, dictionary)
+#     print(mygame.getSolution())
